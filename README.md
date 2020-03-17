b69e5d10d9bcbc18cb9bd9e2672aa37974eac662
Chris Gurney <cgurney@fundthrough.com>
2020-03-17 16:50:02 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Webdev Kit
+
+Very basic starting point for web development.
+
+Does not include web tooling for deployments (minification, etc.)
+
+# Installation
+
+    $ cd /path/to/project
+    $ npm install
+
+
+
+npm install --save-dev gulp
+
+
+
+## Tasks
+
+This section describes the main tasks available. See `gulpfile.js` for subtasks.
+
+### `gulp`
+
+Cleans the output folder (`/dist`), compiles all web files, and runs a local server for development, watching for changes.
---
+++ gulpfile.js
@@ -0,0 +1,170 @@
+var Connect = require('gulp-connect');
+var Del = require('del');
+var Gulp = require('gulp');
+
+/* ************* */
+/* Configuration */
+/* ************* */
+
+var paths = require('./gulpfile.config.json');
+
+/* *************************************************************************
+   Build Tasks
+   ************************************************************************* */
+
+Gulp.task('build:css', function(done) {
+
+  return Gulp.src(paths.src.css)
+    .pipe(Gulp.dest(paths.output.css))
+    .pipe(Connect.reload());
+
+});
+
+Gulp.task('build:html', function(done) {
+
+  return Gulp.src(paths.src.html)
+		.pipe(Gulp.dest(paths.output.base))
+		.pipe(Connect.reload());	 
+
+});
+
+Gulp.task('build:images', function(done) {
+
+	return Gulp.src(paths.src.images)
+		.pipe(Gulp.dest(paths.output.images))
+		.pipe(Connect.reload());
+
+});
+
+Gulp.task('build:js', function(done) {
+
+  return Gulp.src(paths.src.js)
+		.pipe(Gulp.dest(paths.output.js))
+		.pipe(Connect.reload());
+
+});
+
+Gulp.task('build:meta', function(done) {
+
+	return Gulp.src(paths.src.metaFiles, {dot: true})
+		.pipe(Gulp.dest(paths.output.base))
+		.pipe(Connect.reload());
+
+});
+
+Gulp.task('build:vendor', function(done) {
+
+	return Gulp.src(paths.src.vendor)
+		.pipe(Gulp.dest(paths.output.vendor))
+		.pipe(Connect.reload());
+
+});
+
+/* *************************************************************************
+   Watch Tasks
+   ************************************************************************* */
+
+Gulp.task('watch:css', function(done) {
+
+	return Gulp.watch(paths.src.css, 
+		Gulp.series('build:css'));
+
+});
+
+Gulp.task('watch:html', function(done) {
+
+	return Gulp.watch(paths.src.html, 
+		Gulp.series('build:html'));
+
+});
+
+Gulp.task('watch:images', function(done) {
+
+	return Gulp.watch(paths.src.images, 
+		Gulp.series('build:images'));
+
+});
+
+Gulp.task('watch:js', function(done) {
+
+	return Gulp.watch(paths.src.js, 
+		Gulp.series('build:js'));
+
+});
+
+Gulp.task('watch:meta', function(done) {
+
+	return Gulp.watch(paths.src.metaFiles, 
+		Gulp.series('build:meta'));
+
+});
+
+Gulp.task('watch:vendor', function(done) {
+
+	return Gulp.watch(paths.src.vendor, 
+		Gulp.series('build:vendor'));
+
+});
+
+/* *************************************************************************
+   Clean Tasks 
+   ************************************************************************* */
+
+Gulp.task('clean', function(done) {
+  return Del([
+    paths.output.base
+  ]);
+});
+
+/* *************************************************************************
+   Main Tasks
+   ************************************************************************* */
+
+Gulp.task('build',
+	Gulp.parallel(
+		'build:css',
+		'build:html',
+		'build:meta',
+		'build:images',
+		'build:js',
+		'build:vendor'			
+	),
+	function(done) {
+		done();
+	}
+);	
+
+Gulp.task('server', function() {
+
+  Connect.server({
+  	root: paths.output.base,
+  	livereload: true
+  });
+
+});
+
+Gulp.task('watch', 
+	Gulp.parallel(
+		'watch:css',
+		'watch:html',	
+		'watch:images',
+		'watch:js',
+		'watch:meta',
+		'watch:vendor'),
+	function(done) {
+		done();
+	}
+);
+
+Gulp.task('default',
+	Gulp.series(
+		'clean', 
+		'build',
+		Gulp.parallel(
+			'server',
+			'watch'
+		),
+	function(done) {
+		done();
+	})
+);
